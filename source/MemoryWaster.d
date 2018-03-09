6534a5c20744974961c2177581c5a5a09ac272c3
Saurabh Totey <SaurabhTotey@gmail.com>
2018-03-08 20:58:12 -0700

    Added way to stop wasting memory

--- source/MemoryWaster.d
+++ source/MemoryWaster.d
@@ -4,14 +4,16 @@ import core.thread;
 
 /**
  * A function that wastes wasteAmount of bytes, and does so every sleepTime milliseconds
+ * Also takes a reference to a boolean
+ * Function will continue to execute or waste memory until continueWastingMemory is false
  */
-void wasteMemory(int wasteAmount)(ulong sleepTime) {
+void wasteMemory(int wasteAmount)(ulong sleepTime, ref bool continueWastingMemory) {
     byte[wasteAmount] makeAllocation() {
         byte[wasteAmount] wastedSpace;
         return wastedSpace;
     }
     immutable timeStep = msecs(sleepTime);
-    for(;;) {
+    while(continueWastingMemory) {
         makeAllocation();
         Thread.sleep(timeStep);
     }
--- source/app.d
+++ source/app.d
@@ -1,10 +1,18 @@
 module App;
 
 import std.stdio;
+import core.thread;
+import MemoryWaster;
 
 /**
  * Entry point of the program
  */
 void main() {
     writeln("Hello World!");
+    __gshared bool isRunning = true;
+    new Thread({
+        wasteMemory!5(500, isRunning);
+    }).start();
+    Thread.sleep(msecs(5000));
+    isRunning = false;
 }
